94fbbf5004584966c380dd74bd09ce6ea2d9d49b
barbara73 <barbara.jesacher@gmail.com>
2022-09-02 16:55:49 +0200

    delete invalid

--- tests/test_black_out.py
+++ tests/test_black_out.py
@@ -9,9 +9,13 @@ def test_blacken_pixels_returns_same_ds(transfer_syntax_ds):
     assert blacken_pixels(transfer_syntax_ds) == transfer_syntax_ds
 
 
-def test_blacken_pixels_returns_blackened_image_ds_for_agfa(agfa_dataset):
+def test_blacken_pixels_returns_changed_photometric_interpretation(transfer_syntax_ds):
     """Data set changed, when manufacturer is Agfa."""
-    agfa_dataset.Manufacturer = 'Agfa'
+    transfer_syntax_ds.Modality = 'US'
+    transfer_syntax_ds.ManufacturerModelName = 'PHILIPS'
+    transfer_syntax_ds.ImageType = 'DERIVED\PRIMARY\OTHER\VPCT\clablabla'
+    transfer_syntax_ds.PhotometricInterpretation = 'YBR_FULL_422'
+    assert blacken_pixels(transfer_syntax_ds).PhotometricInterpretation == 'RGB'
 
 
 def test_return_unchanged_ds_if_attribute_is_missing(agfa_dataset):
@@ -20,18 +24,18 @@ def test_return_unchanged_ds_if_attribute_is_missing(agfa_dataset):
     assert black_ds.PhotometricInterpretation == 'RGB'
 
 
-def test_delete_dicom_returns_none(a_dataset):
+def test_delete_dicom_returns_true(a_dataset):
     """Return None if data set has specific image type, size and modality."""
-    a_dataset.Manufacturer = 'SIEMENS'
-    assert delete_dicom(a_dataset) is None
+    assert delete_dicom(a_dataset) is True
 
 
-def test_delete_dicom_returns_not_none(a_dataset):
+def test_delete_dicom_returns_true_when_nb_frames_is_none(a_dataset):
     """If one condition is different, then image will not be deleted."""
-    a_dataset.Manufacturer = 'Agfa'
-    assert delete_dicom(a_dataset) is a_dataset
+    a_dataset.NumberOfFrames = None
+    assert delete_dicom(a_dataset) is True
 
 
-def test_delete_dicom_returns_same_ds(a_dataset):
-    """If one condition is different, then image will not be deleted."""
-    assert delete_dicom(a_dataset) is a_dataset
+def test_delete_dicom_returns_false(a_dataset):
+    """Return None if data set has specific image type, size and modality."""
+    a_dataset.ImageType = 'DERIVED\PRIMARY\OTHER\VPCT\clablabla'
+    assert delete_dicom(a_dataset) is False
